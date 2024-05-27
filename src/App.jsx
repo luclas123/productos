5d125169a7d77a9e0f9e2736d8f3d741f1ecb44b
luclas123 <lc2090365@gmail.com>
2024-05-27 19:21:26 -0300

    datos guardados

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Component, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default class App extends Component{
+  
+
+  render(){
+    return(
+    <div>
+      APP
+    </div>
+    )
+  }
+}
